32025be437291ebb948b603d0ce908ba196a98b7
kevin-robles-pacheco <57153502+kevin-robles-pacheco@users.noreply.github.com>
2024-11-02 17:17:11 -0500

    Add Class Cliente And Servidor
    Se agrega la clase Cliente y la clase Servidor al proyecto.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Servidor s = new Servidor();
+        Scanner sc = new Scanner(System.in);
+
+        mostrarTexto("Ingresa el puerto [5050 por defecto]: ");
+        String puerto = sc.nextLine();
+        if (puerto.length() <= 0) puerto = "5050";
+        s.ejecutarConexion(Integer.parseInt(puerto));
+        s.escribirDatos();
+    }
+
+    public static void mostrarTexto(String s) {
+        System.out.print(s);
     }
 }
