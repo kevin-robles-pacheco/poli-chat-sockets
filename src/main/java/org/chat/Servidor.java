18ebd0c875463f8e74d35da65cdeed55a543c274
kevin-robles-pacheco <57153502+kevin-robles-pacheco@users.noreply.github.com>
2024-11-05 20:26:50 -0500

    Rename Package

--- src/main/java/org/chat/Servidor.java
+++ src/main/java/org/chat/Servidor.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.chat;
 
 import java.net.*;
 import java.io.*;
--- src/test/java/org/chat/AppTest.java
+++ src/test/java/org/chat/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.chat;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
